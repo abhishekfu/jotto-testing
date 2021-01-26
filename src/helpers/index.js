9a2357ff4084dc0ede79abb5648098b58bd62a25
abhishekfu <abhishekjha201163@gmail.com>
2021-01-26 23:18:43 +0530

    Added helper function

---
+++ src/helpers/index.js
@@ -0,0 +1,7 @@
+import { func } from "prop-types";
+
+export function getLetterMatchCount(guessedWord,secretWord){
+    const secretLetterSet = new Set(secretWord.split(''));
+    const guessedLetterSet = new Set(guessedWord.split(''));
+    return [...secretLetterSet].filter(letter=>guessedLetterSet.has(letter)).length;
+};
