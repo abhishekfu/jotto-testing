29995cc4320526e130d17695e005a9415864b89b
abhishekfu <abhishekjha201163@gmail.com>
2021-02-10 18:07:11 +0530

    Add reducer

---
+++ src/reducers/successReducer.js
@@ -0,0 +1,11 @@
+/**
+ * @function successReducer
+ * @param {array} state - Array of Guessed words
+ * @param {object} action - action to be reduced
+ * @returns {boolean} - new success state
+ */
+
+
+export default (state,action)=>{
+    return null;
+}
